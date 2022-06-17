4e0da0aaf0229848e9971c3dc0d2d2c515b509c7
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-17 16:21:05 +0200

    Added new action creator to create new stream.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,6 @@
-import { USER_AUTH } from './types';
+import { USER_AUTH, STREAM_CREATE } from './types';
+
+import streamer from '../apis/streamer';
 
 export const setSignIn = (isSignedIn, id) => {
     return {
@@ -9,3 +11,13 @@ export const setSignIn = (isSignedIn, id) => {
         }
     };
 };
+
+export const createStream = (stream) => async dispatch => {
+    await streamer.createStream(stream);
+    dispatch({
+        type: STREAM_CREATE,
+        payload: {
+            stream
+        }
+    });
+}
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,2 @@
 export const USER_AUTH = 'USER_AUTH';
+export const STREAM_CREATE = 'STREAM_CREATE';
