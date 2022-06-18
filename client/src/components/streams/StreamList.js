4f654ddf3d60093659fe1ade98704dd9c37edf40
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-18 09:48:21 +0200

    StreamList component : added 'Create New Stream' button.

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -38,20 +38,34 @@ const StreamList = (props) => {
                 </div>
             </div>
         </div>
-        ));
+    ));
+
+    const renderCreate = () => {
+        if (!props.isSignedIn)
+            return null;
+
+        return <div style={{ textAlign: 'right' }}>
+                <Link to="/streams/new" className="ui button primary">
+                    Create New Stream
+                </Link>
+            </div>;
+    }
+
     return (
         <div>
             <h2>Streams</h2>
             <div className="ui celled list">
                 {renderedStreams}
             </div>
+            {renderCreate()}
         </div>
     );
 };
 
 const mapStateToProps = ({ streams, auth }) => ({
     streams: Object.values(streams),
-    userId: auth.id
+    userId: auth.id,
+    isSignedIn: auth.isSignedIn
 });
 
 export default connect(mapStateToProps, {
