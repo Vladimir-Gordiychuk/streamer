1d8f4146f1ac04e2f46dcf3ee5e5f552d3a26ad5
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-18 16:27:50 +0200

    StreamShow component : wired up stream data to StreamShow component.

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,5 +1,34 @@
-const StreamShow = () => {
-    return <div>StreamShow</div>;
-};
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-export default StreamShow;
+class StreamShow extends React.Component {
+
+    componentDidMount() {
+        if (!this.props.stream && this.props.match.params.id) {
+            this.props.fetchStream(this.props.match.params.id);
+        }
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Stream not found.</div>;
+        }
+
+        return (
+            <div>
+                <h2>{this.props.stream.title}</h2>
+                {this.props.stream.description}
+            </div>
+            );
+    }
+
+}
+
+const mapStateToProps = ({ streams }, ownProps) => ({
+    stream : streams[ownProps.match.params.id]
+});
+
+export default connect(mapStateToProps, {
+    fetchStream
+})(StreamShow);
