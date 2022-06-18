545099f791559b62683fe9296dc2f5217beb84b0
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-18 09:16:32 +0200

    StreamCreate component : binded form submition to action creator (STREAM_CREATE).

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -28,7 +30,7 @@ class StreamCreate extends React.Component {
     }
 
     onSubmit = (formValues) => {
-        console.log(formValues);
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -53,7 +55,11 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'StreamCreate',
     validate
-})(StreamCreate);
+ })(StreamCreate);
+
+export default connect(null, {
+    createStream
+})(formWrapped);
