171e87ec786c7bd433f186b95fd03152996cb051
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-18 12:32:22 +0200

    StreamDelete component : implemented using StreamForm (no read only mode yet).

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,5 +1,37 @@
-const StreamDelete = () => {
-    return <div>StreamDelete</div>;
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, deleteStream } from '../../actions';
+import StreamForm from './StreamForm';
+
+const StreamDelete = (props) => {
+
+    useEffect(() => {
+        if (!props.stream && props.match.params.id) {
+            props.fetchStream(props.match.params.id);
+        }
+    }, []);
+
+    const onSubmit = (formValues) => {
+        props.deleteStream(props.stream.id);
+    };
+
+    if (!props.stream) {
+        return <div>Specified stream not found.</div>
+    }
+
+    return (
+        <div>
+            <h3>Delete Stream</h3>
+            <StreamForm onSubmit={onSubmit} initialValues={props.stream} />
+        </div>
+    );
 };
 
-export default StreamDelete;
+const mapStateToProps = ({ streams }, ownProps) => ({
+    stream: streams[ownProps.match.params.id]
+});
+
+export default connect(mapStateToProps, {
+    fetchStream,
+    deleteStream
+})(StreamDelete);
