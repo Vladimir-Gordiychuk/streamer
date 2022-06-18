d6717af7b51fe4410af1f0eef5a8835f4c1ad65a
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-18 10:20:38 +0200

    StreamEdit component : binded state and action creator to collect all required data.

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,5 +1,26 @@
-const StreamEdit = () => {
-    return <div>StreamEdit</div>;
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchStream } from '../../actions';
+
+const StreamEdit = (props) => {
+
+    useEffect(() => {
+        if (!props.stream && props.match.params.id) {
+            props.fetchStream(props.match.params.id);
+        }
+    }, []);
+
+    if (!props.stream) {
+        return <div>Specified stream not found!</div>
+    }
+    return <div>Edit '{props.stream.title}'</div>;
 };
 
-export default StreamEdit;
+const mapStateToProps = ({ streams }, ownProps) => ({
+        stream: streams[ownProps.match.params.id]
+});
+
+export default connect(mapStateToProps, {
+    fetchStream
+})(StreamEdit);
